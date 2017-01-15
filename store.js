28842b57897a4995e56caf2942c30bff1f1f987f
hsisoft <homointellectus@ya.ru>
2017-01-15 17:39:33 +0300

    1. Code Style Settings 2. Auto Code Reformat using Code Style Settings 3. Change JS version to "ECMAScript 6"

--- store.js
+++ store.js
@@ -4,6 +4,7 @@ function statement(customer, movies) {
 	let totalAmount = 0;
 	let frequentRenterPoints = 0;
 	let result = `Rental Record for ${customer.name}\n`;
+
 	for (let r of customer.rentals) {
 		let movie = movies[r.movieID];
 		let thisAmount = 0;
@@ -36,6 +37,7 @@ function statement(customer, movies) {
 		result += `\t${movie.title}\t${thisAmount}\n`;
 		totalAmount += thisAmount;
 	}
+
 	// add footer lines
 	result += `Amount owed is ${totalAmount}\n`;
 	result += `You earned ${frequentRenterPoints} frequent renter points\n`;
