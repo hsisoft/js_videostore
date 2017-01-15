c115a2ebcd1b39cee7d5856671369f7695ad047b
hsisoft <homointellectus@ya.ru>
2017-01-15 23:34:50 +0300

    Extracted class Rental to Rental js file.

---
+++ rental.js
@@ -0,0 +1,17 @@
+"use strict";
+
+class Rental {
+	constructor(data){
+		this._data = data
+	}
+
+	get movieID() {
+		return this._data.movieID;
+	}
+
+	get days() {
+		return this._data.days;
+	}
+}
+
+module.exports = Rental;
